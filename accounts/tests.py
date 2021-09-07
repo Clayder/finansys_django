13ff39ea2e6eb4e4cd5936c63a0cc0ebfef49417
Peter Clayder <peterclayder@gmail.com>
2021-09-07 07:59:25 -0300

    style

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 
 class AccountTest(TestCase):
     def test_get(self):
-
         """GET /accounts/ must return status code 200"""
         response = self.client.get("/accounts/")
         self.assertEqual(200, response.status_code)
